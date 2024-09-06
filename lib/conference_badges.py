81c9faa0e5d22fef97e471c070161269ba395ac0
Brandon Collins <brandonc503@yahoo.com>
2024-09-05 21:42:46 -0400

    completed lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,38 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+    assignments = []
+    room_index = 1
+
+    for name in names:
+        assignment = f"Hello, {name}! You'll be assigned to room {room_index}!"
+        assignments.append(assignment)
+        room_index += 1
+    
+
+    
+    return assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    
+    output = "\n".join(badges) + "\n"
+    for assignment in room_assignments:
+        output += assignment + "\n"
+    
+    return output
+
+def printer(names):
+    badges = batch_badge_creator(names)
+    assignments = assign_rooms(names)
+    
+    combined_messages = badges + assignments
+    
+    for message in combined_messages:
+        print(message)
